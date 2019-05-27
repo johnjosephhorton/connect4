dc6076d6c404fb9f707b30c023a202c1df1f6db2
John Horton <john.joseph.horton@gmail.com>
2019-05-26 20:21:25 -0400

    First commit

---
+++ connect4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#white = ⚪
+#black = 
+print("|⚫||⚪|🔴")
+print("|🔴|🔴|🔴|⚫|")
+
+#+----+
+#|    |
+#+----+
+# family = ["You", "Sister", "Mom", "Dad"]
+
+# 0 = empty
+# 1 = black
+# 2 = red
+# row = [0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]    
+
+# i = 0
+# sequence = [row[i]] 
+# while i < (len(row) - 1):
+#     if row[i + 1] == row[i]:
+#         sequence.append(row[i + 1])
+#     else:
+#         print(sequence)
+#         sequence = [row[i + 1]]
+#     i = i + 1
+                    
